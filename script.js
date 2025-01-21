d7286d657c5e3a52e5d00d1c9c58d37aee682ae7
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-01-21 05:00:04 +0000

    finished the todo idea

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const  addButton = document.getElementById("add-task-btn");
+    const  taskInput = document.getElementById("task-input");
+    const  taskList = document.getElementById("task-list");
+ 
+
+    function addTask(){
+        const taskText = taskInput.value;
+
+        if(taskText !== ""){
+            const textContent = document.createElement("li");
+            textContent.textContent = taskText;
+            const button = document.createElement("button");
+            button.textContent = "Remove";
+            button.className = 'remove-btn'
+            button.onclick = function(){
+                taskList.removeChild(textContent)
+            }
+            textContent.appendChild(button);
+            taskList.appendChild(textContent);
+
+            taskInput.value = "";
+        }else{
+            alert("please enter a task")
+        }
+    }
+
+    addButton.addEventListener("click",addTask)
+    taskInput.addEventListener("keypress",function(event){
+        if(event.key === "Enter"){
+            addTask()
+        }
+    }
+)
+
+document.addEventListener("DOMContentLoaded",addTask)
+
+})
